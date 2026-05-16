9ee921e7d9c06af1502acf91a3cf07b8dd703517
donstarkey <don.starkey@hotmail.com>
2026-05-15 22:44:52 -0400

    hard coded prod conn string

--- backend/obj/Debug/net8.0/snorkel-explorer.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/snorkel-explorer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("snorkel-explorer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+932652332b1e85ed7c6307f53070ae16f373053c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e57c8218c55425b5e7a545f48934420fff793db")]
 [assembly: System.Reflection.AssemblyProductAttribute("snorkel-explorer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("snorkel-explorer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
